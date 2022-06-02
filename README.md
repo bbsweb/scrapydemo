682d18eae1b27e2fa93ca54c5740705a3e374e51
zero <871159479@qq.com>
2022-06-02 19:52:40 +0800

    add image spider

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ pip install scrapy
 | | 运行 |
 |---|---|
 | [爬取豆瓣电影 Top250](spiders/top250.py) | `scrapy crawl top250` |
+| [爬取图片](spiders/image.py) | `scrapy crawl image` |
---
+++ spiders/image.py
@@ -0,0 +1,25 @@
+import scrapy
+from items import ImageItem
+
+
+class ImageSpider(scrapy.Spider):
+    name = 'image'
+
+    custom_settings = {
+        'IMAGES_STORE': 'images',  # 图片保存路径
+        'ITEM_PIPELINES': {
+            'scrapy.pipelines.images.ImagesPipeline': 300,  # 图片管道
+        },
+    }
+
+    start_urls = ['https://bbsweb.xyz/demo/scrapy-img/']
+
+    def parse(self, response, **kwargs):
+        # 当前页面的图片列表
+        items = response.xpath('//*[@id="__docusaurus"]/div[2]/div/div')
+        # 获取列表内所有图片地址
+        images = items.xpath('div/img/@src').extract()
+
+        # 遍历图片并下载
+        for image in images:
+            yield ImageItem(image_urls=[response.urljoin(image)])
